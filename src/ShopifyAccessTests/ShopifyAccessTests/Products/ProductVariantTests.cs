4a51465b9d8a812dffd36ae9da992f9d10f0707b
Dmitry Sobornykov <d1984v@gmail.com>
2013-08-06 16:08:04 +0300

    removed access token

--- src/ShopifyAccessTests/ShopifyAccessTests/Products/ProductVariantTests.cs
+++ src/ShopifyAccessTests/ShopifyAccessTests/Products/ProductVariantTests.cs
@@ -1,5 +1,4 @@
-﻿using FluentAssertions;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using ShopifyAccess;
 using ShopifyAccess.Models.Configuration.Command;
 using ShopifyAccess.Models.ProductVariant;
@@ -9,18 +8,18 @@ namespace ShopifyAccessTests.Products
 	[ TestFixture ]
 	public class ProductVariantTests
 	{
-		private readonly IShopifyFactory ShopifyFactory = new ShopifyFactory();
-		private const string ShopName = "skuvault";
-		private const string AccessToken = "ce22522b5b2ad8cce975429ec265db4c";
-		private const long VariantId = 337095345;
+		//private readonly IShopifyFactory ShopifyFactory = new ShopifyFactory();
+		//private const string ShopName = "skuvault";
+		//private const string AccessToken = "";
+		//private const long VariantId = 337095345;
 
-		[ Test ]
-		public void ProductVariantQuantityUpdated()
-		{
-			var config = new ShopifyCommandConfig( ShopName, AccessToken );
-			var service = this.ShopifyFactory.CreateService( config );
-			var variantToUpdate = new ShopifyProductVariant { Id = VariantId, Quantity = 100, InventoryManagement = InventoryManagement.Shopify };
-			service.UpdateProductVariantQuantity( variantToUpdate );
-		}
+		//[ Test ]
+		//public void ProductVariantQuantityUpdated()
+		//{
+		//	var config = new ShopifyCommandConfig( ShopName, AccessToken );
+		//	var service = this.ShopifyFactory.CreateService( config );
+		//	var variantToUpdate = new ShopifyProductVariant { Id = VariantId, Quantity = 100, InventoryManagement = InventoryManagement.Shopify };
+		//	service.UpdateProductVariantQuantity( variantToUpdate );
+		//}
 	}
 }
