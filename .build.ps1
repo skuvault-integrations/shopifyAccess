af07821d1d33aa16e9a86b2d4cecb9ff96c4789f
Slav Ivanyuk <slav.isv@gmail.com>
2015-02-12 20:17:45 -0500

    updated packages

--- .build.ps1
+++ .build.ps1
@@ -87,9 +87,9 @@ task NuGet Package, Version, {
 		<tags>$project_short_name</tags>
 		<dependencies> 
 			<group targetFramework="net45">
-				<dependency id="Netco" version="1.3.1" />
+				<dependency id="Netco" version="1.5.0" />
 				<dependency id="CuttingEdge.Conditions" version="1.2.0.0" />
-				<dependency id="ServiceStack.Text" version="4.0.7" />
+				<dependency id="ServiceStack.Text" version="4.0.36" />
 			</group>
 		</dependencies>
 	</metadata>
