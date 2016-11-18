2b3b50bb642a361802f22911d895fe9a2a5c1b6a
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2016-11-18 13:54:05 +0300

    Added throttler

--- .build.ps1
+++ .build.ps1
@@ -87,7 +87,7 @@ task NuGet Package, Version, {
 		<tags>$project_short_name</tags>
 		<dependencies> 
 			<group targetFramework="net45">
-				<dependency id="Netco" version="1.5.3" />
+				<dependency id="Netco" version="1.5.4" />
 				<dependency id="CuttingEdge.Conditions" version="1.2.0.0" />
 				<dependency id="ServiceStack.Text" version="4.0.60" />
 			</group>
---
+++ src/ShopifyAccess/Misc/ShopifyThrottler.cs
@@ -0,0 +1,50 @@
+﻿using Netco.ThrottlerServices;
+
+namespace ShopifyAccess.Misc
+{
+	public class ShopifyThrottler: Throttler
+	{
+		/// <summary>
+		/// Throttler constructor. See code section for details
+		/// </summary>
+		/// <code>
+		/// // Maximum request quota: 20 requests. Restore rate: Five items every second
+		/// var throttler = new Throttler( 20, 1, 5 )
+		/// 
+		/// // Maximum request quota of six and a restore rate of one request every minute
+		/// var throttler = new Throttler( 6, 60, 1 )
+		/// </code>
+		/// <param name="maxQuota">Max quota</param>
+		/// <param name="delayInSecondsBeforeRelease">Delay in seconds before release</param>
+		/// <param name="itemsCountForRelease">Items count for release. Default is 1</param>
+		/// <param name="maxRetryCount">Max Retry Count</param>
+		/// <param name="throttleMessage">Throttle Message</param>
+		public ShopifyThrottler( int maxQuota = 40, int delayInSecondsBeforeRelease = 1, int itemsCountForRelease = 2, int maxRetryCount = 10, string throttleMessage = "Too Many Requests" ):
+			base( maxQuota, el => el / delayInSecondsBeforeRelease * itemsCountForRelease, el => delayInSecondsBeforeRelease - el, maxRetryCount, throttleMessage )
+		{
+		}
+	}
+
+	public class ShopifyThrottlerAsync: ThrottlerAsync
+	{
+		/// <summary>
+		/// Throttler constructor. See code section for details
+		/// </summary>
+		/// <code>
+		/// // Maximum request quota: 20 requests. Restore rate: Five items every second
+		/// var throttler = new Throttler( 20, 1, 5 )
+		/// 
+		/// // Maximum request quota of six and a restore rate of one request every minute
+		/// var throttler = new Throttler( 6, 60, 1 )
+		/// </code>
+		/// <param name="maxQuota">Max quota</param>
+		/// <param name="delayInSecondsBeforeRelease">Delay in seconds before release</param>
+		/// <param name="itemsCountForRelease">Items count for release. Default is 1</param>
+		/// <param name="maxRetryCount">Max Retry Count</param>
+		/// <param name="throttleMessage">Throttle Message</param>
+		public ShopifyThrottlerAsync( int maxQuota = 40, int delayInSecondsBeforeRelease = 1, int itemsCountForRelease = 2, int maxRetryCount = 10, string throttleMessage = "Too Many Requests" ):
+			base( maxQuota, el => el / delayInSecondsBeforeRelease * itemsCountForRelease, el => delayInSecondsBeforeRelease - el, maxRetryCount, throttleMessage )
+		{
+		}
+	}
+}
