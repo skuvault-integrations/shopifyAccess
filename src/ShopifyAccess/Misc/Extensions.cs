58378b8a8642fc1d92986626eb11fad4eeb5045a
Bulat Farrakhov <bulat.farrakhov@skuvault.com>
2020-11-09 16:32:35 +0300

    GUARD-899: removed query part from products images urls;

---
+++ src/ShopifyAccess/Misc/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ShopifyAccess.Misc
+{
+	public static class Extensions
+	{
+		public static string GetUrlWithoutQueryPart( this string rawUrl )
+		{
+			if ( string.IsNullOrWhiteSpace( rawUrl ) )
+				return rawUrl;
+
+			try
+			{
+				var url = new Uri( rawUrl );
+				return url.GetLeftPart( UriPartial.Path );
+			}
+			catch { }
+
+			return rawUrl;
+		}
+	}
+}
