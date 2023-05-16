abbd790cb0b5bf06f9576a93c76879c4cb5c97c7
maxim.saltanov <maxim.saltanov@skuvault.com>
2023-05-16 09:57:15 +0300

    GUARD-2896 whitespaces cleaned up

--- src/ShopifyAccess/Services/WebRequestServices.cs
+++ src/ShopifyAccess/Services/WebRequestServices.cs
@@ -198,7 +198,7 @@ namespace ShopifyAccess.Services
 		{
 			var limit = GetLimitFromHeader( headers );
 			ShopifyLogger.LogGetResponse< T >( uri, limit, content, mark, timeout );
-			
+
 			return !string.IsNullOrEmpty( content ) ? content.FromJson< T >() : default( T );
 		}
 
@@ -208,7 +208,7 @@ namespace ShopifyAccess.Services
 			var nextPageLink = PagedResponseService.GetNextPageQueryStrFromHeader( headers );
 			ShopifyLogger.LogGetResponse< T >( uri, limit, nextPageLink, content, mark, timeout );
 
-			var result = !string.IsNullOrEmpty( content ) ? content.FromJson< T >() : default( T );			
+			var result = !string.IsNullOrEmpty( content ) ? content.FromJson< T >() : default( T );
 
 			return new ResponsePage< T > 
 			{
