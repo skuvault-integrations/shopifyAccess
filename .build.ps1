1b047b5e5176bb23b4e69f4dc963e42e73252f56
Sergey Shumakov <sergey.shumakov@skuvault.com>
2023-06-08 10:33:07 +0300

    GUARD-2632 Update package version

--- .build.ps1
+++ .build.ps1
@@ -76,7 +76,7 @@ task NuGet Package, Version, {
 <package>
 	<metadata>
 		<id>$project_name</id>
-		<version>$Version-alpha.1</version>
+		<version>$Version</version>
 		<authors>SkuVault</authors>
 		<owners>SkuVault</owners>
 		<projectUrl>https://github.com/agileharbor/$project_name</projectUrl>
