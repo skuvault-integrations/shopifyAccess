cd176f8d12f5474ef5412ee6886e45419a6ec42a
Mikhail Rassudishkin <mikhail.rassudishkin@skuvault.com>
2023-08-29 18:35:24 +0300

    VT-6136 : added more tests for Blank

--- src/ShopifyAccessTests/ShopifyAccessTests/Products/Models/ShopifyProductVariantTests.cs
+++ src/ShopifyAccessTests/ShopifyAccessTests/Products/Models/ShopifyProductVariantTests.cs
@@ -8,9 +8,26 @@ namespace ShopifyAccessTests.Products.Models
 	[ TestFixture ]
 	public class ShopifyProductVariantTests
 	{
+		[ TestCase( "" ) ]
+		[ TestCase( null ) ]
+		[ TestCase( "Blank" ) ]
+		[ TestCase( "blank" ) ]
+		[ TestCase( "strange_status" ) ]
+		public void Deserialize_ReturnsBlankInventoryManagement( string rawInventoryManagement )
+		{
+			// Arrange
+			var json = "{\"id\":1,\"inventory_management\":\"" + rawInventoryManagement + "\",\"inventory_item_id\":0,\"weight\":0,\"price\":0,\"updated_at\":\"\\/Date(-62135596800000-0000)\\/\"}";
+
+			// Act
+			var shopifyProductVariant = JsonSerializer.DeserializeFromString< ShopifyProductVariant >( json );
+
+			// Assert
+			shopifyProductVariant.InventoryManagement.Should().Be( InventoryManagementEnum.Blank );
+		}
+
 		[ TestCase( "Shopify" ) ]
 		[ TestCase( "shopify" ) ]
-		public void Deserialize_CorrectInventoryManagement( string rawInventoryManagement )
+		public void Deserialize_ReturnsShopifyInventoryManagement( string rawInventoryManagement )
 		{
 			// Arrange
 			var json = "{\"id\":1,\"inventory_management\":\"" + rawInventoryManagement + "\",\"inventory_item_id\":0,\"weight\":0,\"price\":0,\"updated_at\":\"\\/Date(-62135596800000-0000)\\/\"}";
