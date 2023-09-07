6e316823af7fa5deb3021a427d2b0e95bda7991d
Mikhail Rassudishkin <mikhail.rassudishkin@skuvault.com>
2023-09-07 12:48:10 +0300

    VT-6137 : updated tests

--- src/ShopifyAccessTests/ShopifyAccessTests/Orders/Models/ShopifyOrderTests.cs
+++ src/ShopifyAccessTests/ShopifyAccessTests/Orders/Models/ShopifyOrderTests.cs
@@ -8,15 +8,8 @@ namespace ShopifyAccessTests.Orders.Models
 	[ TestFixture ]
 	public class ShopifyOrderTests
 	{
-		[ TestCase( "", FulfillmentStatusEnum.Undefined ) ]
-		[ TestCase( null, FulfillmentStatusEnum.Undefined ) ]
-		[ TestCase( "Undefined", FulfillmentStatusEnum.Undefined ) ]
-		[ TestCase( "undefined", FulfillmentStatusEnum.Undefined ) ]
-		[ TestCase( "strange_status", FulfillmentStatusEnum.Undefined ) ]
 		[ TestCase( "Fulfilled", FulfillmentStatusEnum.fulfilled ) ]
-		[ TestCase( "fulfilled", FulfillmentStatusEnum.fulfilled ) ]
 		[ TestCase( "Partial", FulfillmentStatusEnum.partial ) ]
-		[ TestCase( "partial", FulfillmentStatusEnum.partial ) ]
 		public void Deserialize_ReturnsFulfillmentStatus_IgnoringCase( string rawFulfillmentStatus, FulfillmentStatusEnum expectedFulfillmentStatus )
 		{
 			// Arrange
@@ -29,20 +22,26 @@ namespace ShopifyAccessTests.Orders.Models
 			shopifyOrder.FulfillmentStatus.Should().Be( expectedFulfillmentStatus );
 		}
 
-		[ TestCase( "", ShopifySourceNameEnum.Undefined ) ]
-		[ TestCase( null, ShopifySourceNameEnum.Undefined ) ]
-		[ TestCase( "Undefined", ShopifySourceNameEnum.Undefined ) ]
-		[ TestCase( "undefined", ShopifySourceNameEnum.Undefined ) ]
-		[ TestCase( "strange_status", ShopifySourceNameEnum.Undefined ) ]
+		[ TestCase( "" ) ]
+		[ TestCase( null ) ]
+		[ TestCase( "strange_status" ) ]
+		public void Deserialize_ReturnsUndefined_WhenFulfillmentStatusIsNotValid( string rawFulfillmentStatus )
+		{
+			// Arrange
+			var json = "{\"id\":0,\"total_price\":0,\"created_at\":\"\\/Date(-62135596800000-0000)\\/\",\"order_number\":0,\"financial_status\":\"Undefined\",\"fulfillment_status\":\"" + rawFulfillmentStatus + "\",\"source_name\":\"web\"}";
+
+			// Act
+			var shopifyOrder = JsonSerializer.DeserializeFromString< ShopifyOrder >( json );
+
+			// Assert
+			shopifyOrder.FulfillmentStatus.Should().Be( FulfillmentStatusEnum.Undefined );
+		}
+
 		[ TestCase( "Web", ShopifySourceNameEnum.web ) ]
-		[ TestCase( "web", ShopifySourceNameEnum.web ) ]
 		[ TestCase( "Pos", ShopifySourceNameEnum.pos ) ]
-		[ TestCase( "pos", ShopifySourceNameEnum.pos ) ]
 		[ TestCase( "IPhone", ShopifySourceNameEnum.iphone ) ]
-		[ TestCase( "iphone", ShopifySourceNameEnum.iphone ) ]
 		[ TestCase( "Android", ShopifySourceNameEnum.android ) ]
-		[ TestCase( "android", ShopifySourceNameEnum.android ) ]
-		public void Deserialize_ReturnsUndefinedSourceName( string rawSourceName, ShopifySourceNameEnum expectedShopifySourceName )
+		public void Deserialize_ReturnsSourceName_IgnoringCase( string rawSourceName, ShopifySourceNameEnum expectedSourceName )
 		{
 			// Arrange
 			var json = "{\"id\":0,\"total_price\":0,\"created_at\":\"\\/Date(-62135596800000-0000)\\/\",\"order_number\":0,\"financial_status\":\"Undefined\",\"fulfillment_status\":\"\",\"source_name\":\"" + rawSourceName + "\"}";
@@ -51,7 +50,22 @@ namespace ShopifyAccessTests.Orders.Models
 			var shopifyOrder = JsonSerializer.DeserializeFromString< ShopifyOrder >( json );
 
 			// Assert
-			shopifyOrder.SourceName.Should().Be( expectedShopifySourceName );
+			shopifyOrder.SourceName.Should().Be( expectedSourceName );
+		}
+
+		[ TestCase( "" ) ]
+		[ TestCase( null ) ]
+		[ TestCase( "strange_status" ) ]
+		public void Deserialize_ReturnsUndefined_WhenSourceNameIsNotValid( string rawSourceName )
+		{
+			// Arrange
+			var json = "{\"id\":0,\"total_price\":0,\"created_at\":\"\\/Date(-62135596800000-0000)\\/\",\"order_number\":0,\"financial_status\":\"Undefined\",\"fulfillment_status\":\"\",\"source_name\":\"" + rawSourceName + "\"}";
+
+			// Act
+			var shopifyOrder = JsonSerializer.DeserializeFromString< ShopifyOrder >( json );
+
+			// Assert
+			shopifyOrder.SourceName.Should().Be( ShopifySourceNameEnum.Undefined );
 		}
 	}
 }
