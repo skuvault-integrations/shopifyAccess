4c0f4413d3cb4d67555ab0e53d49f0ba0a729863
christian-bejarano-skuvault <103965678+christian-bejarano-skuvault@users.noreply.github.com>
2024-03-12 06:01:50 -0500

    GUARD-3342 Update Shopify version to v2023-10 (#43)
    GUARD-3342 Update Shopify version

--- src/ShopifyAccessTests/ShopifyAccessTests/BaseTests.cs
+++ src/ShopifyAccessTests/ShopifyAccessTests/BaseTests.cs
@@ -11,7 +11,7 @@ namespace ShopifyAccessTests
 {
 	public class BaseTests
 	{
-		protected static readonly ShopifyApiVersion ApiVersion = ShopifyApiVersion.V2023_04;
+		protected static readonly ShopifyApiVersion ApiVersion = ShopifyApiVersion.V2023_10;
 		protected readonly IShopifyFactory ShopifyFactory = new ShopifyFactory( ApiVersion );
 		protected ShopifyClientCredentials _clientCredentials;
 		protected IShopifyService Service;
