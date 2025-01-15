fc955779eae6ae75f63d0af83ca6e8c4b69ac1ec
maxim-skuvault <maxim.strelsov@linnworks.com>
2025-01-15 16:14:50 -0500

    GUARD-3717 2 Remove not used ShopifyProductsExtensions; add tests of ToShopifyProductImages

--- src/ShopifyAccess/Models/Product/ShopifyProducts.cs
+++ src/ShopifyAccess/Models/Product/ShopifyProducts.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using ShopifyAccess.Models.ProductVariant;
 
 namespace ShopifyAccess.Models.Product
 {
@@ -10,30 +9,6 @@ namespace ShopifyAccess.Models.Product
 		public ShopifyProducts()
 		{
 			this.Products = new List< ShopifyProduct >();
-		}
-	}
-
-	public static class ShopifyProductsExtensions
-	{
-		//TODO GUARD-3717 [Cleanup] Remove if not used in v1
-		public static IDictionary< string, ShopifyProductVariant > ToDictionary( this ShopifyProducts shopifyInventory )
-		{
-			var inventory = new Dictionary< string, ShopifyProductVariant >();
-			foreach( var product in shopifyInventory.Products )
-			{
-				if( product.Variants == null )
-					continue;
-
-				foreach( var variant in product.Variants )
-				{
-					if( variant == null || variant.Sku == null )
-						continue;
-
-					if( !inventory.ContainsKey( variant.Sku ) )
-						inventory.Add( variant.Sku, variant );
-				}
-			}
-			return inventory;
 		}
 	}
 }
