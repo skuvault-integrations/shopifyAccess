cc446234af1eeb4c55df47cf5eb418ac0d829462
christian-bejarano-skuvault <christian.bejarano@skuvault.com>
2025-09-16 12:18:25 -0500

    GUARD-3719 Update tracking info

--- src/Global/GlobalAssemblyInfo.cs
+++ src/Global/GlobalAssemblyInfo.cs
@@ -22,4 +22,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[ assembly : AssemblyVersion( "1.17.1.0" ) ]
+[ assembly : AssemblyVersion( "1.17.2.0" ) ]
