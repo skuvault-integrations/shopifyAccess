4896c425dce7e6b9efb8c174efbb426df1a46fce
christian-bejarano-skuvault <christian.bejarano@skuvault.com>
2025-09-22 11:09:17 -0500

    GUARD-3719 Add missing field in query

--- src/Global/GlobalAssemblyInfo.cs
+++ src/Global/GlobalAssemblyInfo.cs
@@ -22,4 +22,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[ assembly : AssemblyVersion( "1.17.3.0" ) ]
+[ assembly : AssemblyVersion( "1.17.4.0" ) ]
